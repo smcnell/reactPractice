7248f2b6fac409d6a068fe96484fccad22a8099f
Shannon McNellis <shannmcnelli@gmail.com>
2018-11-24 17:25:05 -0500

    start app

---
+++ src/Component/Form/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Input from '../Input';
+import Button from '../Button';
+
+class Form extends React.Component{
+
+  render() {
+
+    return (
+      <div>
+      <Input></Input>
+      <Button></Button>
+      </div>
+    )
+  }
+}
+
+export default Form;
---
+++ src/Component/Input/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Input extends React.Component{
+
+  render() {
+
+    return (
+      <input placeholder ="type here"></input>
+    )
+  }
+}
+
+export default Input;
